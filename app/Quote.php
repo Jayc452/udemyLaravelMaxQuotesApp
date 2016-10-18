10c39b5df1b640ffa9d90ad5309aa2a862bc1b37
jayc <sanjeethc@gmail.com>
2016-10-18 13:39:38 -0700

    changed the method name to author()

--- app/Quote.php
+++ app/Quote.php
@@ -7,12 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Quote extends Model
 {
     //set relation with Author model
-    //note the name of this function is plural of authors
-    public function authors(){
+    //note the name of this function is singular of author
+   //the  name of this method is important. It is singular because of the 'belongsTo' relationship
+   //if you use plural (authors) as method name here, it will give error
+    public function author(){
     	
     	//we have already set Author to have hasMany relation with Quote model
     	//here we set the inverse of that
     	return $this->belongsTo('App\Author');
+    	
     }
 	
 }
